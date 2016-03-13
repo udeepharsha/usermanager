9df5d7c99a4f9f32c28958d0a9f6f656dd8ff51e
udeepharsha <udeepharsha@gmail.com>
2016-03-12 18:14:40 -0800

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Librarylab</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="../resources/assets/css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../resources/assets/js/jquery.js"></script>
+<script type="text/javascript" src="../resources/assets/js/cufon-yui.js"></script>
+<script type="text/javascript" src="../resources/assets/js/arial.js"></script>
+<script type="text/javascript" src="../resources/assets/js/cuf_run.js"></script>
+<script type="text/javascript" src="../resources/assets/js/radius.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="logo">
+        <h1><a href="index.html">Library<span>lab</span></a></h1>
+      </div>
+      <div class="clr"></div>
+      <div class="menu_nav">
+        <ul>
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a href="{{url('/books')}}">Books</a></li>
+          <li><a href="{{url('/books/about')}}">About Us</a></li>
+          <li><a href="{{url('/books')}}">PDF books</a></li>
+          <li><a href="{{url('/books/contact')}}">Contact Us</a></li>
+        </ul>
+        <div class="search">
+          <form id="form" name="form" method="post" action="#">
+            <span>
+            <input name="q" type="text" class="keywords" id="textfield" maxlength="50" value="Search..." />
+            <input name="b" type="image" src="../resources/assets/images/search.gif" class="button" />
+            </span>
+          </form>
+        </div>
+      </div>
+      <div class="clr"></div>
+      <img src="../resources/assets/images/main_img.jpg" alt="" width="972" height="313" style="padding:25px 0;" />
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="clr"></div>
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar">
+        <div class="article">
+          <h2><span>Template License</span></h2>
+          <div class="clr"></div>
+          <p>Posted on 18. Sep, 2015 by Sara in Filed under templates, internet, with<a href="#"> Comments 18</a></p>
+          <img src="../resources/assets/images/img_1.jpg" width="613" height="193" alt="" />
+          <div class="clr"></div>
+          <p>This is a free CSS website template by AtomicWebsiteTemplates.com.</p>
+        </div>
+        <div class="article">
+          <h2><span>Aliquam Risus</span> Justo</h2>
+          <div class="clr"></div>
+          <p>Posted on 18. Sep, 2015 by Sara in Filed under templates, internet, with <a href="#">Comments 18</a></p>
+          <img src="../resources/assets/images/img_2.jpg" width="613" height="193" alt="" />
+          <div class="clr"></div>
+          <p>Pellentesque posuere enim et ipsum dignissim convallis.</p>
+        </div>
+        <div class="article" style="padding:5px 20px 2px 20px; background:none; border:0;">
+          <p>Page 1 of 2 <span class="butons"><a href="#">3</a><a href="#">2</a> <a href="#" class="active">1</a></span></p>
+        </div>
+      </div>
+      <div class="sidebar">
+        <div class="gadget">
+          <h2>Sidebar Menu</h2>
+          <div class="clr"></div>
+          <ul class="sb_menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">TemplateInfo</a></li>
+            <li><a href="#">Style Demo</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Archives</a></li>
+          </ul>
+        </div>
+        <div class="gadget">
+          <h2><span>Sponsors</span></h2>
+          <div class="clr"></div>
+          <ul class="ex_menu">
+            <li><a href="http://www.dreamtemplate.com">DreamTemplate</a><br />
+              Over 6,000+ Premium Web Templates</li>
+            <li><a href="http://www.templatesold.com/">TemplateSOLD</a><br />
+              Premium WordPress &amp; Joomla Themes</li>
+            <li><a href="http://www.imhosted.com">ImHosted.com</a><br />
+              Affordable Web Hosting Provider</li>
+            <li><a href="http://www.myvectorstore.com">MyVectorStore</a><br />
+              Royalty Free Stock Icons</li>
+            <li><a href="http://www.evrsoft.com">Evrsoft</a><br />
+              Website Builder Software &amp; Tools</li>
+            <li><a href="http://www.csshub.com/">CSS Hub</a><br />
+              Premium CSS Templates</li>
+          </ul>
+        </div>
+      </div>
+      <div class="clr"></div>
+      <div class="fbg">
+        <div class="fbg_resize">
+          <div class="col c1">
+            <h2><span>Image Gallery</span></h2>
+            <a href="#"><img src="../resources/assets/images/gallery_1.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="../resources/assets/images/gallery_2.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="../resources/assets/images/gallery_3.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="../resources/assets/images/gallery_4.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="../resources/assets/images/gallery_5.jpg" width="58" height="58" alt="" /></a> <a href="#"><img src="../resources/assets/images/gallery_6.jpg" width="58" height="58" alt="" /></a> </div>
+          <div class="col c2">
+            <h2><span>My Books</span></h2>
+            <p>Book Stores<br />
+              </p>
+          </div>
+          <div class="col c3">
+            <h2><span>Contact</span></h2>
+            <p>for any help plese contact<a href="#">udeepharsha@example.com</a><br />
+              <a href="#">+1 (123) 444-5677</a></p>
+          </div>
+          <div class="clr"></div>
+        </div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="clr"></div>
+  </div>
+</div>
+<div align=center>Created By UdeepHarsha <a href=''>for EleganceTechnology</a></div></body>
+</html>
